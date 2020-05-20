a6b504c38a94696cf2f98f644c1f17cfeb88fb95
Vicki Cole <hello@vickiccole.com>
2020-05-20 13:25:17 +0100

    Add test for passing in multiple queries

--- 06-anagrams/lib/anagrams.rb
+++ 06-anagrams/lib/anagrams.rb
@@ -1,16 +1,17 @@
-def find_combinations(word, list)
-  list.select do |w|
-    w.chars.all? do |c|
-      char_occurences(c, w).length == char_occurences(c, word).length
+def find_combinations(query_list, master_list)
+  query_list.reduce([]) do |memo, query_word|
+    matches = master_list.select do |master_word|
+      master_word.chars.all? do |c|
+        char_occurences(c, master_word).length == char_occurences(c, query_word).length
+      end
     end
+
+    matches.length > 0 ? memo << matches : memo
   end
 end
+
+private 
 
 def char_occurences(char, word)
   (0 .. word.length).select { |i| word[i,1] == char }
 end
-
-# "pear",
-# "inlets",
-# "listen",
-# "silent"
--- 06-anagrams/test/anagrams_test.rb
+++ 06-anagrams/test/anagrams_test.rb
@@ -3,14 +3,32 @@ require_relative '../lib/anagrams.rb'
 
 class AnagramsTest <  Minitest::Test
   def test_combinations_for_one_word
-    master_list = [ "apple", "enlist" ]
-    combinations = find_combinations("listen", master_list)
-    assert_equal ["enlist"], combinations
+    master_list = [ "apple", "enlist", "listen" ]
+    combinations = find_combinations(["listen"], master_list)
+    assert_equal [["enlist", "listen"]], combinations
   end
 
   def test_combinations_for_substrings
     master_list = ["fresher", "fresh"]
-    combinations = find_combinations("refresher", master_list)
+    combinations = find_combinations(["refresher"], master_list)
     assert_equal [], combinations
+  end
+
+  def test_combinations_for_multiple_words
+    master_list = [
+      "crepitus", "cuprites", "pictures", "piecrust",
+      "enlist", "inlets", "listen", "silent",
+      "labrador", "wallet", "cruciferus"
+    ]
+
+    combinations = find_combinations(["pictures", "enlist"], master_list)
+    assert_equal [
+      [ "crepitus", "cuprites", "pictures", "piecrust" ],
+      ["enlist", "inlets", "listen", "silent" ]
+    ], combinations
+  end
+
+  def test_passing_in_master_list_from_file
+    
   end
 end
