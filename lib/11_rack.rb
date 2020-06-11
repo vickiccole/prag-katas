aa01ceebe118003af3d7416a7859db13f70aa6ff
Vicki Cole <hello@vickiccole.com>
2020-06-11 11:20:00 +0100

    Add quicksort solution

--- lib/11_rack.rb
+++ lib/11_rack.rb
@@ -5,6 +5,27 @@ class Rack
     @balls = []
   end
 
-  def add
+  def add(ball)
+    balls = @balls + [ball]
+    @balls = quicksort(balls)
+  end
+
+  private
+
+  def quicksort(list)
+    if list.size > 1
+      pivot_point = list.size / 2
+
+      val = list[pivot_point]
+
+      left = list.select { |n| n < val }
+      center = list.select { |n| n == val }
+      right = list.select { |n| n > val }
+
+      result = quicksort(left) + center + quicksort(right)
+      result
+    else
+      list
+    end
   end
 end
